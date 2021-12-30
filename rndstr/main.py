2d0bfda3b139d660b2575ed761a4d3d55b81c6d5
Calx <robuk@bokuno.com>
2021-12-30 22:08:25 +0900

    add:コンソールスクリプトで実行できるsetup.pyとREADME.mdに説明を追記

--- rndstr/main.py
+++ rndstr/main.py
@@ -7,6 +7,8 @@ from rndstr import rndstr
 
 def main():
     parser = argparse.ArgumentParser(description='rndstr')
+    # ナンバーを指定しない場合の処理を追加
+    # 現在は指定しないとエラーになるため
     parser.add_argument('arg1')
     args = parser.parse_args()
     specifyNumber = int(sys.argv[1])
